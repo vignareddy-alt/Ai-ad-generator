f0ec54a4f1da8d03d60ef47ac6e2c8ed77bd78c4
Gadeela Vigna <2311cs010203@mallareddyunverdity.ac.in>
2026-03-26 19:16:24 +0530

    again updated the model of hf

--- backend/services/huggingface.js
+++ backend/services/huggingface.js
@@ -8,7 +8,7 @@ ${prompt}
 `;
 
     const response = await fetch(
-      "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2",
+      "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
       {
         method: "POST",
         headers: {
